29d0040e5121f229bab55f9f7e8d352475edf165
davidwt5 <davidwtan75@gmail.com>
2021-08-04 13:14:58 +1000

    functional form submission button

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function addBookToLibrary(book, library) {
     library.push(book);
 }
 
+function deleteAllChildren(node){
+    node.innerHTML = "";
+}
+
 function displayLibrary(library){
     const libraryContainer = document.querySelector('.library');
     for(book of library) {
@@ -49,12 +53,32 @@ function hideForm(){
     form.classList.add("ghost");
 }
 
+function clearForm(){
+    document.querySelector('#title-input').value = "";
+    document.querySelector('#author-input').value = "";
+    document.querySelector('#read-input').checked = false;
+}
+
 const newBookBtn = document.querySelector('.new-book');
 newBookBtn.addEventListener('click', e => bringUpForm());
 
 document.documentElement.addEventListener('keydown', e => {
     if(e.code === "Escape")
         hideForm();
+});
+
+const newBookSubmission = document.querySelector('.new-book-sumbmission');
+newBookSubmission.addEventListener('click', e => {
+    console.log('hi');
+    const title = document.querySelector('#title-input').value;
+    const author = document.querySelector('#author-input').value;
+    const read = document.querySelector('#read-input').checked;
+    const book = new Book(title, author, read);
+    addBookToLibrary(book, myLibrary);
+    deleteAllChildren(document.querySelector('.library'));
+    displayLibrary(myLibrary);
+    clearForm();
+    hideForm();
 });
 
 addBookToLibrary(new Book("LOTR", "J.R.R Tolkien", true), myLibrary);
